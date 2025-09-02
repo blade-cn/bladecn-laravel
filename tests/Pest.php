3ada64c869feb8f8ce4221f3b99378e761cccde3
G4b0rDev <gabordesigns@gmail.com>
2025-09-02 23:15:26 +0200

    feat: setup pestphp

---
+++ tests/Pest.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+use Bladecn\Tests\TestCase;
+
+uses(TestCase::class)->in(__DIR__);
---
+++ tests/TestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bladecn\Tests;
+
+use Bladecn\BladecnServiceProvider;
+use Illuminate\Foundation\Application;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    /** @param Application $app */
+    public function getEnvironmentSetUp($app): void
+    {
+        config()->set('database.default', 'testing');
+    }
+
+    /**
+     * @param  Application  $app
+     * @return array<int, class-string>
+     */
+    protected function getPackageProviders($app): array
+    {
+        return [
+            BladecnServiceProvider::class,
+        ];
+    }
+}
