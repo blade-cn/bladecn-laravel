f6856baced809e8a750dad21bb971786cbd67e12
G4b0rDev <gabordesigns@gmail.com>
2025-09-14 00:46:45 +0200

    fix: update config keys and GitHub raw URL

--- src/ValueObjects/Repo.php
+++ src/ValueObjects/Repo.php
@@ -19,13 +19,13 @@ final readonly class Repo
     public function rawContentUrl(): string
     {
         $branch = match ($this->source) {
-            SourceControl::GITHUB => config('bladecn.source.github.branch', 'main'),
-            SourceControl::GITLAB => config('bladecn.source.gitlab.branch', 'main'),
-            SourceControl::BITBUCKET => config('bladecn.source.bitbucket.branch', 'main'),
+            SourceControl::GITHUB => config('bladecn-laravel.source.github.branch', 'main'),
+            SourceControl::GITLAB => config('bladecn-laravel.source.gitlab.branch', 'main'),
+            SourceControl::BITBUCKET => config('bladecn-laravel.source.bitbucket.branch', 'main'),
         };
 
         return match ($this->source) {
-            SourceControl::GITHUB => "https://raw.githubusercontent.com/{$this->owner}/{$this->repo}/{$branch}",
+            SourceControl::GITHUB => "https://raw.githubusercontent.com/{$this->owner}/{$this->repo}/refs/heads/{$branch}",
             SourceControl::GITLAB => "https://gitlab.com/{$this->owner}/{$this->repo}/-/raw/{$branch}",
             SourceControl::BITBUCKET => "https://bitbucket.org/{$this->owner}/{$this->repo}/raw/{$branch}",
         };
@@ -34,9 +34,9 @@ final readonly class Repo
     public function accessToken(): ?string
     {
         return match ($this->source) {
-            SourceControl::GITHUB => config('bladecn.source.github.token'),
-            SourceControl::GITLAB => config('bladecn.source.gitlab.token'),
-            SourceControl::BITBUCKET => config('bladecn.source.bitbucket.token'),
+            SourceControl::GITHUB => config('bladecn-laravel.source.github.token'),
+            SourceControl::GITLAB => config('bladecn-laravel.source.gitlab.token'),
+            SourceControl::BITBUCKET => config('bladecn-laravel.source.bitbucket.token'),
         };
     }
 }
